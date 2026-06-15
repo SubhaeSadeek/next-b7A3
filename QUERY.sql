e374df7e67aee1f3f2dacd64e9042992c22df0d7
SubhaeSadeek <nurun.nabi@outlook.com>
2026-06-15 20:36:18 +0600

    problem 5 and 6 done

--- QUERY.sql
+++ QUERY.sql
@@ -85,24 +85,88 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (505, 3, 102, 'C-20', 'Pending', 120.00);
 
 
+
 -- =========================================================================
 -- QUERY 1:
 -- =========================================================================
-SELECT match_id, fixture, base_ticket_price FROM Matches 
-  WHERE tournament_category = 'Champions League' 
+SELECT
+  match_id,
+  fixture,
+  base_ticket_price
+FROM
+  Matches
+WHERE
+  tournament_category = 'Champions League'
   AND match_status = 'Available';
 
 
 -- =========================================================================
 -- QUERY 2:
 -- =========================================================================
-SELECT user_id, full_name, email FROM Users 
-  WHERE full_name ILIKE 'Tanvir%'
+SELECT
+  user_id,
+  full_name,
+  email
+FROM
+  Users
+WHERE
+  full_name ILIKE 'Tanvir%'
   OR full_name ILIKE '%Haque%';
+
 
 -- =========================================================================
 -- QUERY 3:
 -- =========================================================================
+SELECT
+  booking_id,
+  user_id,
+  match_id,
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM
+  Bookings
+WHERE
+  payment_status IS NULL;
 
-SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status FROM Bookings 
-  WHERE payment_status IS null;
+
+-- =========================================================================
+-- QUERY 4:
+-- =========================================================================
+SELECT
+  b.booking_id,
+  u.full_name,
+  m.fixture,
+  b.total_cost
+FROM
+  Bookings AS b
+  JOIN Users AS u ON b.user_id = u.user_id
+  JOIN Matches AS m ON b.match_id = m.match_id;
+
+-- =========================================================================
+-- QUERY 5:
+-- =========================================================================
+SELECT
+  u.user_id,
+  u.full_name,
+  b.booking_id
+FROM
+  Users AS u
+  LEFT JOIN Bookings AS b ON u.user_id = b.user_id;
+
+-- =========================================================================
+-- QUERY 6:
+-- =========================================================================
+SELECT
+  booking_id,
+  match_id,
+  total_cost
+FROM
+  Bookings
+WHERE
+  total_cost > (
+    SELECT
+      AVG(total_cost)
+    FROM
+      Bookings
+  );
+
+  
